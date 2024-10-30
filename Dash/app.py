3e19296ee2c4e7f61d5673c8c35d6c6976703939
wolderufael <wolderuphaelkassahun@gmail.com>
2024-10-30 14:00:27 +0300

    app.py edit

--- Dash/app.py
+++ Dash/app.py
@@ -1,12 +1,26 @@
+# from app_instance import app  # Import the app instance
+# from layout import create_layout  # Import the layout
+# # from form import form_layout
+# import callbacks  # Import callbacks to register them
+
+
+# # Assign the layout from layout.py
+# app.layout = create_layout(app)
+# # app.form=form_layout(app)
+# # Run the app
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
+
 from app_instance import app  # Import the app instance
 from layout import create_layout  # Import the layout
-# from form import form_layout
 import callbacks  # Import callbacks to register them
-
+import os  # Import os to access environment variables
 
 # Assign the layout from layout.py
 app.layout = create_layout(app)
-# app.form=form_layout(app)
-# Run the app
+
+# Run the app on the specified port and host
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    # Use Render's dynamically assigned port
+    port = int(os.getenv("PORT", 8000))  # Fallback to 8000 if $PORT is not set
+    app.run_server(host="0.0.0.0", port=port, debug=True)
